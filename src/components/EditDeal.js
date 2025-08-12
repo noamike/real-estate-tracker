6d24c98014222c29eb361d68afe6bb7894221852
Noah <noahmichaels@gmail.com>
2025-08-11 23:26:43 -0400

    created editDeal

---
+++ src/components/EditDeal.js
@@ -0,0 +1,16 @@
+import {collection, query, where, getDocs, doc, updateDoc} from "firebase/firestore";
+import useAuth from "../hooks/useAuth";
+import {db} from "../firebase/FirebaseConfig";
+import {useState} from "react";
+
+const EditDeal = () => {
+
+
+    return(
+        <div>
+            
+        </div>
+    );
+};
+
+export default EditDeal;
