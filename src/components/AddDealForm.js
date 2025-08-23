531230386b3261a832e0712f09c424a7172ff379
kevinharps <33207609+kevinharps@users.noreply.github.com>
2025-08-23 16:03:12 -0400

    Added New Form Elements
    Updated AddDealForm.js with new fields and logic.

--- src/components/AddDealForm.js
+++ src/components/AddDealForm.js
@@ -91,15 +91,9 @@ const AddDeal = () =>{
     return(
         <form onSubmit={handleSubmit}>
             <h2>Add a New Deal</h2>
-            <div>
-                Choose Lease or Sale: 
-                <select id="leaseOrSale" name="leaseOrSale" value={dealData.leaseOrSale} onChange={handleChange} required>
-                    <option value="lease">Lease</option>
-                    <option value="sale">Sale</option>
-                </select>
-            </div>
-            <div>
-                Deal Type: 
+
+             <div>
+                Deal Type:  <nbsp/>
                 <select id="dealType" name="dealType" value={dealData.dealType} onChange={handleChange} required>
                     <option value="none">-</option>
                     <option value="office">Office</option>
@@ -107,8 +101,21 @@ const AddDeal = () =>{
                     <option value="industrial">Industrial</option>
                 </select>
             </div>
+
+            <div>Choose Lease or Sale: <nbsp/>
+                {/* <select id="leaseOrSale" name="leaseOrSale" value={dealData.leaseOrSale} onChange={handleChange} required>
+                    <option value="lease">Lease</option>
+                    <option value="sale">Sale</option>
+                </select> */}
+                <input type="radio" id="leaseOrSale" value="lease" name="leaseOrSale" onChange={handleChange}/>
+                <label for="lease">Lease</label>
+                <input type="radio" id="leaseOrSale" value="sale" name="leaseOrSale" onChange={handleChange}/>
+                <label for="sale">Sale</label>
+            </div>
+
+           
             <div>
-                Building Name: 
+                Building Name: <nbsp/>
                 <input
                     type="text"
                     name='dealName'
@@ -119,7 +126,7 @@ const AddDeal = () =>{
                 />
             </div>
             <div>
-                Building Address:
+                Building Address: <nbsp/>
                 <input
                     type="text"
                     name='buildingAddress'
@@ -129,6 +136,90 @@ const AddDeal = () =>{
                     required
                 />
             </div>
+            
+
+            {/* Lease-specific fields */}
+            {dealData.leaseOrSale === 'lease' && (
+            <div>
+                <h4>Lease Details</h4>
+                <div>Lease Term & Rate: <nbsp/>
+                    <input
+                        type="text"
+                        name="leaseTerm"
+                        value={dealData.leaseTerm}
+                        onChange={handleChange}
+                        placeholder="Lease Term"
+                    />
+                    <input
+                    type="text"
+                    name="leaseRate"
+                    value={dealData.leaseRate}
+                    onChange={handleChange}
+                    placeholder="Lease Rate"
+                    />
+                </div>
+                <div>Lease Start and End Date: <nbsp/>
+                <input
+                    type="date"
+                    name="leaseDate"
+                    value={dealData.leaseDate}
+                    onChange={handleChange}
+                    placeholder="Lease Start Date"
+                />
+                <input
+                    type="date"
+                    name="leaseExpiration"
+                    value={dealData.leaseExpiration}
+                    onChange={handleChange}
+                    placeholder="Lease Expiration"
+                />
+                </div>
+                <div>Additional Charges: <nbsp/>
+                <input
+                    type="text"
+                    name="additionalCharges"
+                    value={dealData.additionalCharges}
+                    onChange={handleChange}
+                    placeholder="Additional Charges"
+                />
+                </div>
+            </div>
+        )}
+
+        {/* Sale-specific fields */}
+        {dealData.leaseOrSale === 'sale' && (
+            <div>
+                <h4>Sale Details</h4>
+                <input
+                    type="text"
+                    name="sellerName"
+                    value={dealData.sellerName}
+                    onChange={handleChange}
+                    placeholder="Seller Name"
+                />
+                <input
+                    type="date"
+                    name="saleDate"
+                    value={dealData.saleDate}
+                    onChange={handleChange}
+                    placeholder="Sale Date"
+                />
+                <input
+                    type="number"
+                    name="salePrice"
+                    value={dealData.salePrice}
+                    onChange={handleChange}
+                    placeholder="Sale Price"
+                />
+                <input
+                    type="number"
+                    name="ppsq"
+                    value={dealData.ppsq}
+                    onChange={handleChange}
+                    placeholder="Price Per Sq Ft"
+                />
+            </div>
+        )}
             <h3>Deal will be saved with user {userID} as main Agent</h3>
             <button type="submit">Submit</button>
         </form>
